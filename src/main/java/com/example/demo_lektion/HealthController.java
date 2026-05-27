da57db191c1444178e3164ae84a8c76706882fc4
kimmo <kimmo.ahola@systementor.se>
2026-05-27 13:32:43 +0200

    fixed files used in project

--- src/main/java/com/example/demo_lektion/HealthController.java
+++ src/main/java/com/example/demo_lektion/HealthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+    // Vi vill kunna köra ett snabbt test och se om appen är live efter deploy
     @GetMapping("/health")
     public String health() {
         return "ok";
